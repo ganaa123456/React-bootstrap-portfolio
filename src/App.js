c61de00294f585f6124f17deee390bd5c94f5f54
ganaa <ganaa.12396@yahoo.com>
2022-11-15 15:29:44 +0800

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import React from "react";
+import NavbarMenu from "./Components/Navbar/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./Components/Header/Header";
+import About from "./Components/About/About";
+import Service from "./Components/Service/Service";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
+import { Routes, Route } from "react-router-dom";
+import "./Responsive/Responsive.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavbarMenu />
+      <Routes>
+        <Route path="" element={<Header />} />
+        <Route path="about" element={<About />} />
+        <Route path="service" element={<Service />} />
+        <Route path="contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
